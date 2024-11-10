f81c7ef6212867e6ae9f41a53e3b46d1b6288d7f
BenKielinski <benjamin.kielinski@hotmail.fr>
2024-11-10 16:38:23 +0100

    add mention legales

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-# Installer les dépendances
-echo "Installing dependencies..."
-npm install
-
-# Construire le projet (si nécessaire)
-echo "Building the project..."
-npm run build
-
-echo "Deployment completed!"
